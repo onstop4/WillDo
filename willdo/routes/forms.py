2dcfb9668be6d1ff02a1701032c3391d86cd92fa
Andrew Henkel <search1230@gmail.com>
2020-07-17 18:13:58 -0400

    Fix keys in the form response not matching WTForms forms.
    A function and a dictionary were added so that the data in
    ImmutableMultiDict from flask.request.form (which contains
    the form inputs) are usable in the WTForms form
    subclasses. This is meant to resolve the scenario where
    the keys present in the flask.request.form don't match the
    attributes in the WTForms form subclasses (which is the
    case for TaskForm).

--- willdo/routes/forms.py
+++ willdo/routes/forms.py
@@ -7,6 +7,31 @@ BASIC_INPUT_REGEX = r'^.*\w.*$'
 PRIORITY_INPUT_REGEX = r'^[A-Z]$'
 DATE_INPUT_REGEX = r'^\d{4}-\d{2}-\d{2}$'
 
+key_association = {
+    'completion-date': 'completion_date',
+    'creation-date': 'creation_date'
+}
+
+
+def convert_associated_keys(form_input: MultiDict):
+    """Returns a :class:`MultiDict` with keys converted according to
+    :obj:`key_association`. The original keys are still present,
+    along with any keys not found in :obj:`key_association`. This
+    function is meant to resolve the scenario where the keys present
+    in the form response don't match the attributes in the WTForms
+    :class:`Form` classes.
+
+    :param MultiDict form_input: The submitted form inputs.
+    :return: A MultiDict with the old and the new ("converted") keys.
+    :rtype: MultiDict
+    """
+
+    new_form_input = form_input.copy()
+    for key in form_input.keys():
+        new_key = key_association.get(key, key)
+        new_form_input[new_key] = form_input[key]
+    return new_form_input
+
 
 class TasklistForm(Form):
     name = StringField(validators=[InputRequired(), Regexp(BASIC_INPUT_REGEX)])
@@ -31,6 +56,8 @@ def validate_tasklist(form_input: MultiDict):
     :rtype: bool
     """
 
+    form_input = MultiDict(form_input)
+    form_input = convert_associated_keys(form_input)
     form = TasklistForm(form_input)
     return form.validate()
 
@@ -43,5 +70,7 @@ def validate_task(form_input: MultiDict):
     :rtype: bool
     """
 
+    form_input = MultiDict(form_input)
+    form_input = convert_associated_keys(form_input)
     form = TaskForm(form_input)
     return form.validate()
