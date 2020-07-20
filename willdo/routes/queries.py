4a21e9d7624e3b42af61ed0dc77652593d85c69d
Andrew Henkel <search1230@gmail.com>
2020-07-20 19:02:48 -0400

    Implement better queries for db when searching.
    Better methods of querying the database were implemented
    for searching for tasklists and tasks. Prior to this, the
    SQLAlemchemy "ilike" operator was being used for
    searching. This caused problems when search terms contained
    characters like "%" or "_", which functioned as wildcards
    in the generated sql statement. Now, I am using the
    "contains" operator with the autoescape ability turned on,
    meaning that these characters will be treated as literals,
    not wildcards. I also made sure that these queries are
    case-insensitive when performed.

--- willdo/routes/queries.py
+++ willdo/routes/queries.py
@@ -1,24 +1,55 @@
 from flask import g
+from sqlalchemy import func
 from ..db import AvailableTasklist, IndividualTask
 
 
 def query_tasklists(search_for=None):
+    """Returns a query object for the rows of tasklists in
+    the database. If :arg:`search_for` is truthy, the query
+    is filtered for results that contain :arg:`search_for`
+    (case-insensitive).
+
+    :param str search_for: A string that the query is
+                           filtered by.
+    :return: Query object for tasklists.
+    :rtype: sqlalchemy.orm.query.Query
+    """
+
     db_session = g.db_session
-    query = db_session.query(AvailableTasklist).order_by(AvailableTasklist.name)
-    
+    query = db_session.query(AvailableTasklist).order_by(
+        AvailableTasklist.name)
+
     if search_for:
-        query = query.filter(AvailableTasklist.name.ilike('%{}%'.format(search_for)))
-    
+        search_for = search_for.lower()
+        operation = func.lower(AvailableTasklist.name).contains(
+            search_for, autoescape=True)
+        query = query.filter(operation)
+
     return query
 
 
 def query_tasks(tasklist, search_for=None):
+    """Returns a query object for the rows of tasks in
+    the database. If :arg:`search_for` is truthy, the query
+    is filtered for results that contain :arg:`search_for`
+    (case-insensitive).
+
+    :param str search_for: A string that the query is
+                           filtered by.
+    :return: Query object for tasklists.
+    :rtype: sqlalchemy.orm.query.Query
+    """
+
     db_session = g.db_session
-    query = db_session.query(IndividualTask).join(AvailableTasklist).filter(IndividualTask.tasklist_id == tasklist.id)
+    query = db_session.query(IndividualTask).join(
+        AvailableTasklist).filter(IndividualTask.tasklist_id == tasklist.id)
     query = query.order_by(IndividualTask.completion_date)
 
     if search_for:
-        query = query.filter(IndividualTask.description.ilike('%{}%'.format(search_for)))
+        search_for = search_for.lower()
+        operation = func.lower(IndividualTask.description).contains(
+            search_for, autoescape=True)
+        query = query.filter(operation)
 
     return query
 
@@ -31,6 +62,7 @@ def get_tasklist_by_id(tasklist_id):
 def get_task_by_id(task_id):
     db_session = g.db_session
     return db_session.query(IndividualTask).filter_by(id=task_id).first()
+
 
 def get_multiple_tasks_by_id(task_ids):
     for task_id in task_ids:
